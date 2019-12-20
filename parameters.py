891b823ad56a37a419f67114ea191dc3743bcc05
keiju.sato <keiju.sato@abeja.asia>
2019-12-20 16:00:31 +0900

    stop tracking .png and .pt files

--- parameters.py
+++ parameters.py
@@ -3,7 +3,7 @@ import argparse
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--epochs', type=int, default=100)
-parser.add_argument('--lr', type=float, default=0.001)
+parser.add_argument('--lr', type=float, default=0.01)
 parser.add_argument('--save-model', action='store_true', default=False)
 
 args = parser.parse_args()
